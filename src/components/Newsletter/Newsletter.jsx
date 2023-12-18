3d218a2d20a4dc3e7b8b21d54e93a6efdbd4f874
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-12-18 01:11:35 +0100

    commented unused lines

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,4 +1,4 @@
-import css from './FAQ.module.scss';
+// import css from './FAQ.module.scss';
 
 export const FAQ = () => {
   return <section></section>;
--- src/components/OneClick/OneClick.jsx
+++ src/components/OneClick/OneClick.jsx
@@ -1,4 +1,4 @@
-import css from './OneClick.module.scss';
+// import css from './OneClick.module.scss';
 
 export const OneClick = () => {
   return <section></section>;
